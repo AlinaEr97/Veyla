b6259a90bc936a563b31c7a1a562533dd51ce9d0
AlinaEr97 <86489788+AlinaEr97@users.noreply.github.com>
2022-05-03 20:56:15 +0300

    Исправления хедера главной страницы и каталога

--- app/js/catalog/catalog.js
+++ app/js/catalog/catalog.js
@@ -11111,9 +11111,23 @@ $('.application__form').submit(function(e) {
 	}
 });
 
-$(function() {
-	$(".menu-burger").on("click", function() {
-		$(".menu").toggleClass('menu_active-catalog');
+// Показать-скрыть меню в мобильной версии
+
+let burger_button = $(".menu-burger");
+let menu = $(".menu");
+
+burger_button.on("click", function () {
+	menu.fadeToggle();
+	$(".menu-burger__line").each(function() {
+		$(this).toggleClass("menu-burger__line_active");
 	});
 });
 
+$(document).mouseup(function (e) {
+	if ( !burger_button.is(e.target) && burger_button.has(e.target).length === 0 && !menu.is(e.target) && menu.has(e.target).length === 0) {
+		menu.fadeOut();
+		$(".menu-burger__line").each(function() {
+			$(this).removeClass("menu-burger__line_active");
+		});
+	};
+});
--- app/js/menu.js
+++ app/js/menu.js
@@ -1,6 +1,21 @@
-$(function() {
-	$(".menu-burger").on("click", function() {
-		$(".menu").toggleClass('menu_active');
+// Показать-скрыть меню в мобильной версии
+
+let burger_button = $(".menu-burger");
+let menu = $(".menu");
+
+burger_button.on("click", function () {
+	menu.fadeToggle();
+	$(".menu-burger__line").each(function() {
+		$(this).toggleClass("menu-burger__line_active");
 	});
+});
+
+$(document).mouseup(function (e) {
+	if ( !burger_button.is(e.target) && burger_button.has(e.target).length === 0 && !menu.is(e.target) && menu.has(e.target).length === 0) {
+		menu.fadeOut();
+		$(".menu-burger__line").each(function() {
+			$(this).removeClass("menu-burger__line_active");
+		});
+	};
 });
 
